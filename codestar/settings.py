ca4f74dabf8599dc4ba0e327ed4693030e45f618
daswinkelmann <daswinkelmann@gmail.com>
2025-11-19 14:56:35 +0000

    updated ALLOWED_HOSTS with local and 127.0.0.1

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%ffy9!n!+ysjc^@ol(2)*0-2#g6vlp3ctwfgk5_y$cect2@w3n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
