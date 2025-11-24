02c51d4119fd4bef98cc5ae42fa3c0d572f852f9
daswinkelmann <daswinkelmann@gmail.com>
2025-11-24 12:24:27 +0000

    added CSS and template files

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,5 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=0)
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
